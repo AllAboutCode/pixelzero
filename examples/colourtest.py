3eeb6b953ca782e5ba3cf2ebf57cf25d5d2e5b67
Joshua Lowe <josh@edublocks.org>
2018-08-06 18:16:51 +0200

    Create colourtest.py

---
+++ examples/colourtest.py
@@ -0,0 +1,26 @@
+import pixelzero
+import time
+
+pixelno = 8
+n = pixelzero.NeoPixel(18, pixelno)
+
+while True:
+    for r in range(pixelno):
+        n[r] = (255,0,0)
+        n.show()
+
+    time.sleep(1)
+    
+    for g in range(pixelno):
+        n[g] = (0,255,0)
+         n.show()
+
+    time.sleep(1)
+    
+    for b in range(pixelno):
+        n[b] = (0,0,255)
+        n.show()
+    
+    time.sleep(1)
+
+    
